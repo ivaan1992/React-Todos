50ab7045706687e72931a1ee3ad4b7174301ee58
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-13 12:07:17 -0600

    Add remove buttons and form to add tasks

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,16 @@ import React from "react";
 export default function TodoItem(props) {
   return ( 
     <li>
-     <input type="checkbox" /> {props.todo.completed} 
+     <input 
+       type="checkbox" 
+       checked={props.todo.completed}
+       onChange={() => props.handleChangeProps(props.todo.id)}
+      /> 
+     
+      <button onClick={() => props.deleteTodoProps(props.todo.id)}> 
+        Delete
+      </button>
+      {props.todo.title}
     </li>
   )
 }
